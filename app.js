86252eff16b36db3bb53e8e883beb14eb909cb80
Tahaarshad787 <tahaarshad311@gmail.com>
2024-09-12 16:09:52 +0500

    add files and folders

---
+++ app.js
@@ -0,0 +1,296 @@
+// Chapter 5 (MATH EXPRESSIONS)
+// Q1
+
+// var num1 = 3;
+// var num2 = 5
+// var sum = 3 + 5;
+
+// document.write("Sum of " + num1 + " " + "and" + " " + num2 + " is" + " " + sum);
+
+// Q2
+
+// var num1 = 3;
+// var num2 = 5
+// var sum = 3 - 5;
+
+// document.write("Sum of " + num1 + " " + "and" + " " + num2 + " is" + " " + sum);
+
+// var num1 = 3;
+// var num2 = 5
+// var sum = 3 * 5;
+
+// document.write("Sum of " + num1 + " " + "and" + " " + num2 + " is" + " " + sum);
+
+// var num1 = 5;
+// var num2 = 3;
+// var sum = 5 / 3;
+
+// document.write("Sum of " + num1 + " " + "and" + " " + num2 + " is" + " " + sum);
+
+// var num1 = 3;
+// var num2 = 5
+// var sum = 3 % 5;
+
+// document.write("Sum of " + num1 + " " + "and" + " " + num2 + " is" + " " + sum);
+
+// Q3 (A)
+
+// var value;
+
+// // Q3 (B)
+// document.write("Value after variable declaration is undefined" );
+
+// Q3 (C)
+// var num = 5;
+
+// Q3 (D)
+// document.write("Initial value: " + num);
+
+// Q3 (E)
+// var value = 5
+// document.write("increment the variable ", value++ + "<br>");
+
+// Q3 (F)
+// document.write("Value after increment is:" + " ", value + "<br>");
+
+// Q3 (G)
+// var value = 6 + 7
+
+// Q3 (H)
+// document.write("Value after addition is: " + value);
+
+// Q3 (I)
+// var value = 13
+// document.write("Decrement the variable ", value-- + "<br>");
+
+// Q3 (J)
+// document.write("Value after decrement is:" + " ", value);
+
+// Q3 (K)
+// var value = 12 % 3;
+// Q3 (L)
+// document.write("The remainder is : ", value);
+
+// Q4
+// var ticketPrice = 600;
+// var numberOfTickets = 5;
+// var totalCost = ticketPrice * numberOfTickets;
+// document.write("Total cost to buy " + numberOfTickets + " " + "tickets to a movie is ", totalCost + "PKR");
+
+// Q5
+// document.write("<h3>Table of 4</h3>");
+// var table = 4 * 1
+// document.write("4 x 1 = " + table + "<br><br>");
+// var table = 4 * 2
+// document.write("4 x 2 = " + table + "<br><br>");
+// var table = 4 * 3
+// document.write("4 x 3 = " + table + "<br><br>");
+// var table = 4 * 4
+// document.write("4 x 4 = " + table + "<br><br>");
+// var table = 4 * 5
+// document.write("4 x 5 = " + table + "<br><br>");
+// var table = 4 * 6
+// document.write("4 x 6 = " + table + "<br><br>");
+// var table = 4 * 7
+// document.write("4 x 7 = " + table + "<br><br>");
+// var table = 4 * 8
+// document.write("4 x 8 = " + table + "<br><br>");
+// var table = 4 * 9
+// document.write("4 x 9 = " + table + "<br><br>");
+// var table = 4 * 10
+// document.write("4 x 10 = " + table + "<br>");
+
+// Q6
+// var celsiusTemperature = 25; 
+// var fahrenheitTemperature = (celsiusTemperature * 9/5) + 32;
+// document.write(celsiusTemperature + "°C" + " " + "is" + " " , fahrenheitTemperature + "°F" + "<br>");
+// var fahrenheitTemperatureInput = 70;
+// var celsiusTemperatureOutput = (fahrenheitTemperatureInput - 32) * 5/9;
+// document.write(fahrenheitTemperatureInput + "°F" + " " + "is" + " " , celsiusTemperatureOutput + "°C");
+
+// Q7
+// document.write("<h2>Shopping Cart</h2>")
+// var a = 650
+// document.write("Price of item 1 is " + a + "<br>");
+// var c = 3
+// document.write("Quantity of item 1 is " + c + "<br>");
+// var b = 100
+// document.write("Price of item 2 is " + b + "<br>");
+// var d = 7
+// document.write("Quantity of item 2 is " + d +"<br>");
+// var e = 7;
+// document.write("Shipping charges " + e +"<br><br>");
+// var total = 2750
+// document.write("Total cost of your order is " + total);
+
+// Q8
+// document.write("<h2>Marks Sheet</h2>");
+// var total = 980;
+// document.write("Total Marks " + total + "<br>");
+// var obtained = 804;
+// document.write("Marks obtained " + obtained + "<br>");
+// var percentage = obtained / total * 100;
+// document.write("Percentage " + percentage + "<br>");
+
+// Q9
+// var usdToPkrRate = 104.80;
+// var sarToPkrRate = 28;
+// var usdAmount = 10;
+// var sarAmount = 25;
+// var totalPkr = usdAmount * usdToPkrRate + sarAmount * sarToPkrRate;
+// document.write("Total Currency in PKR: " + totalPkr);
+
+// Q10 (A)
+// var initialNumber = 8; 
+// var result = initialNumber + 5 * 10 / 2;
+// document.write(result);
+
+// Q11 
+// document.write("<h2>Age Calculator</h2>");
+// var year = 2016;
+// document.write("Current year " + year + "<br>");
+// var birth = 1992;
+// document.write("Current year " + birth + "<br>");
+// var result = 2016 - 1992;
+// document.write("Your age " + result);
+
+// Q12
+// document.write("<h2>The Geometrizer</h2>");
+// var radius = 20;
+// document.write("Radius of a circle: " + radius + "<br>");
+// var pi = 3.142;
+// var circumference = 2 * pi * radius;
+// document.write("The circumference is: " + circumference + "<br>");
+// var area = pi * radius * 20 ;
+// document.write("The area is: "+ area);
+
+// Q13
+// document.write("<h2>The Lifetime Supply Calculator</h2>");
+// var favoriteSnack = "chocolate chips";
+// document.write("Favourite snack: " + favoriteSnack + "<br>");
+// var currentAge = 15;
+// document.write("Current age: " + currentAge + "<br>");
+// var maximumAge = 65;
+// document.write("Estimated Maximum: " + maximumAge + "<br>");
+// var amountPerDay = 3;
+// document.write("Amount of snacks per day: " + amountPerDay + "<br>");
+// var yearsLeft = maximumAge;
+// var totalAmount = yearsLeft * amountPerDay; 
+// document.write("You will need " +totalAmount + " " + "chocolate chip to last you until the ripe old age of " + yearsLeft);
+
+// Chapter 6-9
+
+// Q1
+// document.write("Result: <br>");
+// var a = 10;
+// document.write("The value of a is: " + a + "<br><br>");
+
+// document.write("..............................<br><br>");
+// document.write("The value of ++a is: ", ++a + "<br>");
+// document.write("Now the value of a is: ", a + "<br><br><br>");
+
+// document.write("The value of a++ is: ", a++ + "<br>");
+// document.write("Now the value of a is: ", a + "<br><br><br>");
+
+// document.write("The value of --a is: ", --a + "<br>");
+// document.write("Now the value of a is: ", a + "<br><br><br>");
+
+// document.write("The value of a-- is: ", a-- + "<br>");
+// document.write("Now the value of a is: ", a + "<br><br><br>");
+
+// Q2
+// var a = 2;
+// var b = 1;
+// var num = --a - --b + ++b + b--;
+// var result = 1 - 0 + 1 + 1;
+// document.write("a is 1 (after --a) <br>");
+// document.write("b is 0 (after --b , ++b and b--) <br>");
+// document.write("result is 3");
+
+// Q3
+// var name = prompt("Enter your name");
+// var user = prompt("Hello! nice to meet you");
+// console.log(name , user);
+
+// Chapter6
+// Q1
+// var x = x + 1;
+// var result = x++;
+
+// Q2
+// var x = 100;
+// var result = --x
+// document.write(result); 
+
+// Q3
+// var x = 50;
+// var y = x++;
+// document.write(y);
+
+// Q4
+// var y = 50;
+// var z = --y
+// document.write(z);
+
+// Q5
+// var num = 10; 
+// var newNum = num--;
+// document.write(newNum);
+
+// Q6
+// var originalValue = 10; 
+// var newlvalue = ++originalValue;
+// document.write(newlvalue);
+
+// Q7
+// var value = 10;
+// var value= ++value;
+// alert("The new value is: " + value);
+
+// Chapter 7
+
+// Q1
+// var calculatedNum = 2 + (2 * 6);
+// var calculatedNum = 14;
+
+// Q2
+// var calculatedNum = (2 + 2) * 6;
+// var calculatedNum = 24;
+
+// Q3
+// var calculatedNum = (2 + 2) * (4 + 2);
+// var calculatedNum = 4 * 6;
+// var calculatedNum = 24;
+
+// Q4
+var calculatedNum = ((2 + 2) * 4) + 2;
+var calculatedNum = (4 * 4) + 2;
+var calculatedNum = 16 + 2;
+var calculatedNum = 18;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
